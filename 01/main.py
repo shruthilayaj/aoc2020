05bdcd1f438a526709f2ef810f8bac523fb0ca2f
Shruthilaya Jaganathan <s3jaganathan@gmail.com>
2020-12-09 11:38:56 +0530

    add day 9

--- 01/main.py
+++ 01/main.py
@@ -9,22 +9,22 @@ def read_input():
     return data
 
 
-def report_repair(data):
+def report_repair(data, target):
     for x in data:
-        remainder = 2020 - x
+        remainder = target - x
         if remainder in data:
             return x * remainder
 
 
 def report_repair_2(data):
-    for i, num_1 in enumerate(data):
+    for i, num_1 in enumerate(data, target):
         for j, num_2 in enumerate(data[i + 1:]):
-            remainder = 2020 - (num_1 + num_2)
+            remainder = target - (num_1 + num_2)
             if remainder in data[i + j + 2:]:
                 return num_1 * num_2 * remainder
 
 
 if __name__ == "__main__":
     data = [int(v) for v in read_input()]
-    print(f"Part 1: {report_repair(data)}")
-    print(f"Part 2: {report_repair_2(data)}")
+    print(f"Part 1: {report_repair(data, 2020)}")
+    print(f"Part 2: {report_repair_2(data, 2020)}")
---
+++ 09/main.py
@@ -0,0 +1,58 @@
+import os
+import re
+
+
+class SumNotFound(Exception):
+    pass
+
+
+def read_input():
+    filename = os.path.join(os.path.dirname(__file__), "input.txt")
+    with open(filename) as f:    
+        data = f.readlines()
+
+    return data
+
+
+def encoding_error(records):
+    sliding_window = records[:25]
+
+    def get_sum(target):
+        for i, x in enumerate(sliding_window):
+            remainder = target - x
+            if remainder in sliding_window[i:]:
+                return
+        else:
+            raise SumNotFound
+
+    for num in records[25:]:
+        try:
+            get_sum(num)
+        except SumNotFound:
+            return num
+        else:
+            sliding_window.pop(0)
+            sliding_window.append(num)
+
+
+def encoding_error_2(target):
+    sliding_window = records[:2]
+
+    i = 2
+    while True:
+        if sum(sliding_window) == target:
+            return min(sliding_window) + max(sliding_window)
+        if sum(sliding_window) < target:
+            sliding_window.append(records[i])
+            i += 1
+        elif sum(sliding_window) > target:
+            sliding_window.pop(0)
+
+
+if __name__ == "__main__":
+    records = [int(x) for x in read_input()]
+
+    target = encoding_error(records)
+    print(f"Part 1: {target}")
+    print(f"Part 2: {encoding_error_2(target)}")
+
